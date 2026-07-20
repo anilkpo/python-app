cc6ad14cc1bd3867e4d51108af0afb7ae6ea932a
anilkpo <ponnam.anilkumar@tecnotree.com>
2026-07-20 17:55:38 +0530

    added host entry for app

--- src/app.py
+++ src/app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
+@app.route('/api/details')
 def get_details():
     return {'message': 'Hello, this is the details endpoint!'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5001,debug=True)
     
