c323d57e77176e979e4f31c1426be2ec22329091
anilkpo <ponnam.anilkumar@tecnotree.com>
2026-07-20 18:09:21 +0530

    added json message as reponse

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify 
 
 app = Flask(__name__)
 
 @app.route('/api/details')
 def get_details():
-    return {'message': 'Hello, this is the details endpoint!'}
+    json_data = {
+        'name': 'Anil',
+        'age': 30,
+        'email': 'anil.p@tecnotree.com'}
+    return jsonify(json_data)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5001,debug=True)
